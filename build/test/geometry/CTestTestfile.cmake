267ce8dfee37e9a386a3f59c193621869b788c97
julian <julianneeleman@hotmail.com>
2017-11-21 20:46:05 +0100

    Don't version build

--- build/test/geometry/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/julian/projects/turbo/test/geometry
-# Build directory: /home/julian/projects/turbo/build/test/geometry
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(geometry_test "geometry_test")
